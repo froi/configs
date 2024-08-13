4d5575225811dffd32636ed404b3e51afbf1e108
Froilán Irizarry Rivera <1918027+froi@users.noreply.github.com>
2024-08-13 11:39:32 -0400

    Update adding dark-notify and wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,10 +8,21 @@ function scheme_for_appearance(appearance)
 	end
 end
 
+wezterm.on("window-config-reloaded", function(window, pane)
+	local overrides = window:get_config_overrides() or {}
+	local appearance = window:get_appearance()
+	local scheme = scheme_for_appearance(appearance)
+	if overrides.color_scheme ~= scheme then
+		overrides.color_scheme = scheme
+		window:set_config_overrides(overrides)
+	end
+end)
+
 return {
 	-- ...your existing config
 	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 	font = wezterm.font("FiraCode Nerd Font"),
 	font_size = 16.0,
 	scrollback_lines = 10000,
+	hide_tab_bar_if_only_one_tab = true,
 }
