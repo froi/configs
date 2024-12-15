f03017702e605627fdd1eac6ad21366ee5a93bdb
Froilán Irizarry Rivera <1918027+froi@users.noreply.github.com>
2024-12-14 21:27:25 -0500

    Update how the rancher desktop path is used

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,14 +1,12 @@
-export PATH="$PATH:/Users/froi/Library/Application Support/JetBrains/Toolbox/scripts"
+RANCHER_BIN="/Users/froi/.rd/bin"
 export GPG_TTY=$TTY
 export EDITOR="nvim"
 export VISUAL="nvim"
 export ADOTDIR="$ZDOTDIR/antigen"
-### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
-export PATH="/Users/froi/.rd/bin:$PATH"
-### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 export PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+export PATH="$RANCHER_BIN:$PATH:/Users/froi/Library/Application Support/JetBrains/Toolbox/scripts"
 export PATH="$(brew --prefix postgresql@16)/bin:$PATH"
 
 [ -f "${ZDOTDIR}/secrets.zsh" ] && source "${ZDOTDIR}/secrets.zsh" 
