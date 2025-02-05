8ab10de98945684ba98d6b9df68a59535a1e89a7
Froilán Irizarry Rivera <code@froi.dev>
2025-02-05 15:56:24 +0100

    Update config with new key bindings and dead keys
    Added a new key bindding to rename the active tab.
    Added config to have wezterm accept dead keys using Mac's OPT key. This
    makes writing accents in WezTerm and NVIM possible.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -21,6 +21,18 @@ function key_bindings()
 			mods = "LEADER",
 			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
 		},
+		{
+			key = "r",
+			mods = "LEADER|OPT",
+			action = wezterm.action.PromptInputLine({
+				description = "Enter new name for tab",
+				action = wezterm.action_callback(function(window, pane, line)
+					if line then
+						window:active_tab():set_title(line)
+					end
+				end),
+			}),
+		},
 		{ key = "t", mods = "LEADER|OPT", action = wezterm.action.ShowTabNavigator },
 		{ key = "h", mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Left") },
 		{ key = "l", mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Right") },
@@ -34,8 +46,10 @@ config.leader = { key = "'", mods = "CMD", timeout_milliseconds = 3000 }
 config.keys = key_bindings()
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 config.font = wezterm.font("FiraCode Nerd Font")
-
 config.font_size = 16.0
 config.scrollback_lines = 10000
 config.hide_tab_bar_if_only_one_tab = true
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
+config.enable_kitty_graphics = true
 return config
