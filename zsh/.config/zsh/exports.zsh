34b5bcad6da26e39399cdbaed7c76bb6e5177197
Froilán Irizarry Rivera <code@froi.dev>
2025-03-01 20:18:35 -0500

    Fix path string and remove empty line

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -5,9 +5,8 @@ postgres_bin="$(brew --prefix postgresql@17)/bin"
 
 export LDFLAGS="-L/opt/homebrew/opt/postgresql@17/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/postgresql@17/include"
-export PATH="$home_local_bin:$home_bin:$postgres_bin$PATH"
+export PATH="$home_local_bin:$home_bin:$postgres_bin:$PATH"
 export POETRY_CONFIG_DIR="$XDG_CONFIG_HOME/pypoetry"
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 export PIPENV_PYTHON="$PYENV_ROOT/shims/python"
-
