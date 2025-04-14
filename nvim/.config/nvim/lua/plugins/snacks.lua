ae3f689c89e2f3e43a9307fb5c6ebf78fee0682d
Froilán Irizarry Rivera <code@froi.dev>
2025-04-14 09:21:14 -0400

    Additional configs for snacks and treesitter

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,22 @@
+return {
+	"folke/snacks.nvim",
+	opts = {
+		-- need notifier for disabling "No notifications available"
+		notifier = { enabled = true },
+
+		image = { enabled = true },
+		lazygit = { enabled = true },
+		picker = {
+			sources = {
+				files = {
+					hidden = true,
+					ignored = true,
+				},
+				explorer = {
+					hidden = true,
+					ignored = true,
+				},
+			},
+		},
+	},
+}
