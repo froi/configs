3159a03758792d67aaafdc5eec9b94946fcf9f64
Froilán Irizarry Rivera <code@froi.dev>
2025-07-08 10:23:07 -0400

    Commet firfox dap out

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,69 +1,69 @@
 local function merge(...)
-	local result = {}
-	for _, tbl in ipairs({ ... }) do
-		for _, value in ipairs(tbl) do
-			table.insert(result, value)
-		end
-	end
-	return result
+  local result = {}
+  for _, tbl in ipairs({ ... }) do
+    for _, value in ipairs(tbl) do
+      table.insert(result, value)
+    end
+  end
+  return result
 end
 
 local daps = {
-	"js-debug-adapter",
-	"firefox-debug-adapter",
-	"go-debug-adapter",
-	"bash-debug-adapter",
+  "js-debug-adapter",
+  -- "firefox-debug-adapter",
+  "go-debug-adapter",
+  "bash-debug-adapter",
 }
 local lsps = {
-	"basedpyright",
-	"bash-language-server",
-	-- "deno",
-	"docker-compose-language-service",
-	"dockerfile-language-server",
-	"eslint-lsp",
-	"gitlab-ci-ls",
-	"gopls",
-	"html-lsp",
-	"htmx-lsp",
-	"json-lsp",
-	"marksman",
-	"prisma-language-server",
-	"protols",
-	"rust-analyzer",
-	"sqls",
-	"templ",
-	"terraform-ls",
-	"typescript-language-server",
-	"vue-language-server",
-	"yaml-language-server",
+  "basedpyright",
+  "bash-language-server",
+  -- "deno",
+  "docker-compose-language-service",
+  "dockerfile-language-server",
+  "eslint-lsp",
+  "gitlab-ci-ls",
+  "gopls",
+  "html-lsp",
+  "htmx-lsp",
+  "json-lsp",
+  "marksman",
+  "prisma-language-server",
+  "protols",
+  "rust-analyzer",
+  "sqls",
+  "templ",
+  "terraform-ls",
+  "typescript-language-server",
+  "vue-language-server",
+  "yaml-language-server",
 }
 local linters = {
-	"protolint",
-	"jsonlint",
-	"htmlhint",
-	"gitlint",
-	"eslint_d",
-	"gitleaks",
-	"sqlfluff",
-	"tflint",
-	"yamllint",
-	"shellcheck",
-	"markdownlint-cli2",
-	"cspell",
+  "protolint",
+  "jsonlint",
+  "htmlhint",
+  "gitlint",
+  "eslint_d",
+  "gitleaks",
+  "sqlfluff",
+  "tflint",
+  "yamllint",
+  "shellcheck",
+  "markdownlint-cli2",
+  "cspell",
 }
 local formatters = {
-	"yamlfmt",
-	"sqlfmt",
-	"prettier",
-	"markdown-toc",
-	"goimports",
-	"markdownlint-cli2",
-	"shfmt",
-	"stylua",
+  "yamlfmt",
+  "sqlfmt",
+  "prettier",
+  "markdown-toc",
+  "goimports",
+  "markdownlint-cli2",
+  "shfmt",
+  "stylua",
 }
 return {
-	"williamboman/mason.nvim",
-	opts = {
-		ensure_installed = merge(lsps, formatters, linters, daps),
-	},
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = merge(lsps, formatters, linters, daps),
+  },
 }
